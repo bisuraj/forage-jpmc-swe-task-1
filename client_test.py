67f6ba525610b3c12d43d1f90ad157c0cdb3eca3
Bisuraj Sharma <53631121+bisuraj@users.noreply.github.com>
2023-06-27 20:37:56 +0545

    Complete

--- client_test.py
+++ client_test.py
@@ -9,6 +9,9 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote),((quote["stock"],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2)))
+
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
@@ -16,10 +19,22 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
+    
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote),((quote["stock"],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+
+quote['top_ask']['price'])/2)))
+
 
   """ ------------ Add more unit tests ------------ """
 
+def test_getRatio(self):
+    prices=[121.2,121.68,119.2,121.68,117.7,119.1,120.09,118.8,120.45,145.56,0,34.4]
+    for i in range(0,len(prices),2):
+       self.assertEqual((prices[i]/prices[i+1]) ,getRatio(prices[i],prices[i+1]))
 
+  #testing  get_ratio function if none is return when divide by zero  
+def test_getRatio_zeroDivisionError(self):
+       self.assertEqual(None ,getRatio(121.1,0))
 
 if __name__ == '__main__':
     unittest.main()
